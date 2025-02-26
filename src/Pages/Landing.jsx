aedd0012e11f08438da3a2ed92af9098badb3214
Merin-jai <145257949+Merin-jai@users.noreply.github.com>
2025-02-26 17:44:30 +0530

    Landing component 3

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Intro from '../components/Landing_Intro';
 import NewDrops from '../components/Landing_NewDrops';
+import Categories from '../components/Landing_Categories';
 import '../Styles/Landingpage.css'
 
 const Landing = () => {
@@ -11,6 +12,7 @@ const Landing = () => {
         <Header />
         <Intro/>
         <NewDrops/>
+        <Categories/>
         <Footer/>
     </div>
   )
